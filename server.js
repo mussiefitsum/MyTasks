4ffef823e6c6bcdfecaefd3942f934f577a03b28
Mussie <mussie.fitsum@gmail.com>
2022-01-11 19:49:34 -0500

    first commit (React + Express)

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+})
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Listening on port ${ port }`);
+})
